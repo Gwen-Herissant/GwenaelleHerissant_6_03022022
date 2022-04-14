f8b07f736219c898e62d55fd56ad4c3365cc7777
Gwenaëlle Herissant <gh@pointligneplan.fr>
2022-04-14 10:38:08 +0200

    [script] full static photographer page

--- scripts/factories/photographer-media.js
+++ scripts/factories/photographer-media.js
@@ -1,15 +1,16 @@
 function galleryFactory(data) {
 
-    let mediaFactory = new MediaFactory();
-
     function mediaCard() {
         const article = document.createElement('article');
 
         const link = document.createElement('a');
-        link.setAttribute('title', `${data.name}`);
-        link.setAttribute('ariaLabel', `${data.name}`);
+        link.setAttribute('title', `${data.title}`);
+        link.setAttribute('ariaLabel', `${data.title}`);
+        link.setAttribute('href', `#`);
         article.appendChild(link);
 
+        let mediaFactory = new MediaFactory(data);
+        console.log(mediaFactory);
         link.append(mediaFactory);
 
         const mediaDetails = document.createElement('div');
@@ -44,35 +45,39 @@ function galleryFactory(data) {
     return { mediaCard };
 }
 
-class MediaImage {
-    mediaElement(data) {
+
+class Image {
+    constructor(data) {
+        this.title = data.title
+        this.image = data.image;
+
         const imageElement = document.createElement('img');
-        imageElement.setAttribute("src", `assets/images/${data.image}`);
-        imageElement.setAttribute('alt', data.title);
+        imageElement.setAttribute("src", `assets/images/${this.image}`);
+        imageElement.setAttribute('alt', this.title);
+        imageElement.classList.add('media-grid__image');
+        console.log(imageElement);
         return imageElement;
     }
 }
 
-class MediaVideo {
-    mediaElement(data) {
+class Video {
+    constructor(data) {
+        this.video = data.video;
+
         const videoElement = document.createElement('video');
-        videoElement.setAttribute("src", `assets/images/${data.video}`);
+        videoElement.setAttribute("src", `assets/images/${this.video}`);
         videoElement.setAttribute('controls', 'controls');
+        videoElement.classList.add('media-grid__video');
         return videoElement;
     }
 }
 
 class MediaFactory {
-    mediaFactory(data) {
-        //let item = data.media;  
-        if (data.hasOwnAttribute('image')) {
-            return new MediaImage(data);
-        } else if (data.hasOwnAttribute('video')) {
-            return new MediaVideo(data);
-        } else {
-            throw 'Format inconnu';
+    constructor(data) {
+        if (data.video === undefined) {
+            return new Image(data);
+        } else if (data.image === undefined) {
+            return new Video(data);
         }
     }
 }
-
-
